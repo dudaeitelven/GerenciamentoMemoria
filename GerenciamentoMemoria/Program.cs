77eade761cf482b7067f813d0f4a341c1a8898b6
Alex <alex_eitelven@hotmail.com>
2019-11-20 20:50:51 -0300

    v1

--- GerenciamentoMemoria/Program.cs
+++ GerenciamentoMemoria/Program.cs
@@ -11,17 +11,54 @@ namespace GerenciamentoMemoria
     {
         static void Main(string[] args)
         {
-            string pagina1;
-            string pagina2;
-            string pagina3;
-            List<String> entradas         = new List<String>();
-            List<String> historiocPagina1 = new List<String>();
-            List<String> historiocPagina2 = new List<String>();
-            List<String> historiocPagina3 = new List<String>();
- 
-            new Thread(() => {
+            int pagina1 = 0;
+            int pagina2 = 0;
+            int pagina3 = 0;
+            List<int> entradas         = new List<int>();
+            List<int> historiocPagina1 = new List<int>();
+            List<int> historiocPagina2 = new List<int>();
+            List<int> historiocPagina3 = new List<int>();
+
+
+            entradas.Add(66);
+            entradas.Add(55);
+            entradas.Add(44);
+            entradas.Add(33);
+            entradas.Add(22);
+            foreach (int number in entradas)
+            {
+                if (pagina1 != 0)
+                {
+
+                }
+                else if (pagina2 != 0) {
+
+                }
+                else if (pagina3 != 0)
+                {
+
+                }
+               
+
+
+
+
+
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+            //new Thread(() => {
                 
-            });
+            //});
 
         }
 
